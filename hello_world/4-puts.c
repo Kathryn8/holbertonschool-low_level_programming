5d2a75d9d95062db0c7d2ac99fa0e24ade7acb7b
Kathryn6368 <6368@holbertonstudents.com>
2023-02-16 18:29:15 -0800

    add tast4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* string initialisation */
+	char MyString[] = "\"Programming is like building a multilingual puzzle";
+
+	/* puts function - prints a string */
+	puts(MyString);
+
+	return (0);
+}
