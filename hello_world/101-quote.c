301a839eec567c37d8f183ee41ab3b3422dd2872
Kathryn6368 <6368@holbertonstudents.com>
2023-02-18 16:07:44 -0800

    add file task 8

---
+++ hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <sys/syscall.h>
+
+/**
+ * main - Entry point
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	const char quote[] = "and that piece of art is useful\" - Dora Korpar, \
+2015-10-19\n";
+	syscall(SYS_write, 1, quote, sizeof(quote));
+	return (1);
+}
