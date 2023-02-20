2634a6b7a80eed5a34ff30fb864b90e90fc69342
Kathryn6368 <6368@holbertonstudents.com>
2023-02-19 20:46:52 -0800

    add file task 8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = '0';
+	while (c != ':')
+	{
+		putchar(c);
+		c += 1;
+	}
+	c = 'a';
+	while (c != 'g')
+	{
+		putchar(c);
+		c += 1;
+	}
+	putchar('\n');
+	return (0);
+}
