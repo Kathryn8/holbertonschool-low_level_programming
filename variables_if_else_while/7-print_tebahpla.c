8f8fe4bcac35965c15f7c67f4ae5d8d1eb9688fa
Kathryn6368 <6368@holbertonstudents.com>
2023-02-19 20:21:45 -0800

    add file task 7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while (c != '`')
+	{
+		putchar(c);
+		c -= 1;
+	}
+	putchar('\n');
+	return (0);
+}
