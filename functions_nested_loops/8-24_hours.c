535420f01d33172398f0c3ad8503cf6fda363590
Kathryn6368 <6368@holbertonstudents.com>
2023-02-22 15:11:45 -0800

    add task 8 file

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * jack_bauer - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void jack_bauer(void)
+{
+	int hours_counter;
+	int seconds_counter;
+	int units_hour;
+	int tens_hour;
+	int units_second;
+	int tens_second;
+
+	hours_counter = 0;
+	while (hours_counter < 24)
+	{
+		seconds_counter = 0;
+		while (seconds_counter < 60)
+		{
+			if (hours_counter == 0)
+			{
+				_putchar(hours_counter + '0');
+				_putchar(hours_counter + '0');
+			} else
+			{
+				units_hour = hours_counter % 10;
+				tens_hour = (hours_counter - units_hour) / 10;
+				_putchar(tens_hour + '0');
+				_putchar(units_hour + '0');
+			}
+			_putchar(':');
+			if (seconds_counter == 0)
+			{
+				_putchar(seconds_counter + '0');
+				_putchar(seconds_counter + '0');
+			} else
+			{
+				units_second = seconds_counter % 10;
+				tens_second = (seconds_counter - units_second) / 10;
+				_putchar(tens_second + '0');
+				_putchar(units_second + '0');
+			}
+			_putchar('\n');
+			seconds_counter = seconds_counter + 1;
+		}
+		hours_counter = hours_counter + 1;
+	}
+}
