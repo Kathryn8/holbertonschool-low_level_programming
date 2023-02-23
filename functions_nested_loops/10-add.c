c129ddba8be76f799a82520258a6bfbe1b05e8e9
Kathryn6368 <6368@holbertonstudents.com>
2023-02-22 17:31:45 -0800

    add task 10 file

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * @a: integer
+ * @b: integer
+ *
+ * Return: result (Success)
+ */
+
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
