f6df9d8da663034f522d87f57516252bee61d19d
Kathryn6368 <6368@holbertonstudents.com>
2023-02-24 02:52:29 -0800

    add task 10 file

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int tens;
+	int units;
+
+	tens = 0;
+	while (tens <= 9)
+	{
+		units = tens + 1;
+		while (units <= 9)
+		{
+			putchar(tens + '0');
+			putchar(units + '0');
+			putchar(',');
+			putchar(' ');
+			units = units + 1;
+		}
+		tens = tens + 1;
+	}
+	putchar('\n');
+	return (0);
+}
