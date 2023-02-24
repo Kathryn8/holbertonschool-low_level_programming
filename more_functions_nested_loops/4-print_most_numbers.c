39a5fa8b57f3e92fc8755249104aefa82ddc023e
Kathryn6368 <6368@holbertonstudents.com>
2023-02-23 21:06:54 -0800

    add file for task 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Entry point
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (i <= 9)
+	{
+		if ( i == 2 || i == 4)
+		{
+			i = i + 1;
+		} else
+		{
+			_putchar(i + '0');
+			i = i + 1;
+		}
+	}
+	_putchar('\n');
+}
