146ef8f50e783f6ee72110220ae0234c8d1abdb4
Kathryn6368 <6368@holbertonstudents.com>
2023-02-23 20:02:01 -0800

    add file for task 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - Entry point
+ * @a: integer
+ * @b: integer
+ *
+ * Return: result (Success)
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
