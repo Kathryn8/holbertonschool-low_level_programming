e96788005e0acfaeac744cecfe1b1cf16b03dd7f
Kathryn6368 <6368@holbertonstudents.com>
2023-02-27 20:19:00 -0800

    add file

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strlen - length of a string
+ * @s: takes parameter pointer to a char
+ *
+ * Return: length of a string without null character
+ */
+
+int _strlen(char *s)
+{
+	int counter;
+
+	counter = 0;
+	while (s[counter] != '\0')
+	{
+		counter = counter + 1;
+	}
+	return (counter);
+}
+
+/**
+ * _strcpy - copies the string pointed to by SRC to DEST
+ * @dest: pointer to a buffer
+ * @src: a string
+ *
+ * Return: pointer to string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	char *tmp;
+
+	tmp = src;
+	src = dest;
+	dest = tmp;
+	return (dest);
+}
