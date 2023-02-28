eac404c6bdd874fdd1be41f8c3e2b4160481c7ba
Kathryn6368 <6368@holbertonstudents.com>
2023-02-27 17:32:22 -0800

    add file

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * _strlen - length of a string
+ * @s: takes parameter pointer to a char
+ *
+ * Return: length of a string without null character
+ */
+
+int _strlen(char *s)
+{
+	int counter;
+
+	counter = 0;
+	while (s[counter] != '\0')
+	{
+		counter = counter + 1;
+	}
+	return (counter);
+}
+
+/**
+ * puts_half - print he second half of a string
+ * @str: takes parameter pointer to a char
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int i;
+
+	if (_strlen(str) % 2 == 0)
+	{
+		i = _strlen(str) / 2;
+	} else
+	{
+		i = (_strlen(str) - 1) / 2;
+	}
+	while (i < _strlen(str))
+	{
+		_putchar(str[i]);
+		i = i + 1;
+	}
+	_putchar('\n');
+}
