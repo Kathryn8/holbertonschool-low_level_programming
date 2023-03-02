4f692a4840280b9d6c3f1292ec1dfa87fb400832
Kathryn6368 <6368@holbertonstudents.com>
2023-03-02 14:58:07 -0800

    add file

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reverse_array  - reverse the content of an array of integers
+ * @a: pointer to an integer
+ * @n: an integer
+ *
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+
+	i = 1;
+	while (i <= n)
+	{
+		printf("%d", a[n - i]);
+		if (i != n)
+			printf(", ");
+		i = i + 1;
+	}
+	printf("\n");
+}
