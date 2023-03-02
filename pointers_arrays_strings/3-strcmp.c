ebd215fc79ba85208f523a0656cd830739c1edde
Kathryn6368 <6368@holbertonstudents.com>
2023-03-02 13:53:28 -0800

    ts

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -31,13 +31,13 @@ int _strcmp(char *s1, char *s2)
 {
 	int s1_len;
 	int s2_len;
-	
+
 	s1_len = _strlen(s1);
 	s2_len = _strlen(s2);
-	if (s1_len == s2_len)
-		return 0;
+	if (s1_len > s2_len)
+		return (-1 * s1_len);
 	else if (s1_len < s2_len)
-		return s1_len*(-3);
+		return (s1_len);
 	else
-		return s1_len*3;
+		return (0);
 }
