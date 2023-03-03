202846dbfc43270acc58e3a94c5b8032b467109a
Kathryn6368 <6368@holbertonstudents.com>
2023-03-02 17:33:15 -0800

    add file

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper  - change all lowercase letter to uppercase
+ * @: pointer to a char
+ *
+ * Return: a string or pointer to a char
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+		i = i + 1;
+	}
+	return (s);
+}
