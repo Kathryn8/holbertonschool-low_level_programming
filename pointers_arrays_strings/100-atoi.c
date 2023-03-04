96fcefc4947a4d501f27a9d079228f20832a65f2
Kathryn6368 <6368@holbertonstudents.com>
2023-03-04 11:23:33 -0800

    add file

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -3,25 +3,6 @@
 #include <stdlib.h>
 
 /**
- * _strlen - length of a string
- * @s: takes parameter pointer to a char
- *
- * Return: length of a string without null character
- */
-
-int _strlen(char *s)
-{
-	int counter;
-
-	counter = 0;
-	while (s[counter] != '\0')
-	{
-		counter = counter + 1;
-	}
-	return (counter);
-}
-
-/**
  * _isdigit - tests a char to see if its a digit between 0 and 9.
  * @c: a char
  *
@@ -41,27 +22,73 @@ int _isdigit(char c)
 }
 
 /**
- * rev_string - reverse a string
- * @s: takes parameter pointer to a char
+ * skip_it -baltantly copied from Jonathan's live coding becuase my/
+ * "first_num" function wasn't working
+ * @str: takes parameter pointer to a char
  *
- * Return: void
+ * Return: pointer to a char
  */
 
-void rev_string(char *s)
+char *skip_it(char *str)
 {
-	int length;
-	int tmp;
-	int q;
-
-	length = _strlen(s) - 1;
-	q = 0;
-	while (q <= (length / 2))
+	while (*str != '\0')
 	{
-		tmp = s[q];
-		s[q] = s[length - q];
-		s[length - q] = tmp;
-		q = q + 1;
+		if (_isdigit(*str))
+		{
+			return (str);
+		}
+		str = str + 1;
 	}
+	return (str);
+}
+
+/**
+ * first_num - find first number in a string
+ * @str: takes parameter pointer to a char
+ *
+ * Return: pointer to a char
+ */
+
+char *first_num(char *str)
+{
+	int i;
+	char *num;
+
+	num = str;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (_isdigit(str[i]) == 1 && _isdigit(str[i + 1]) == 0)
+		{
+			num[0] = str[i];
+			break;
+		}
+		i = i + 1;
+	}
+	return (num);
+}
+
+/**
+ * minus_counter - count the number of minus signs in a string
+ * @str: takes parameter pointer to a char
+ *
+ * Return: integer
+ */
+
+int minus_counter(char *str)
+{
+	int i = 0;
+	int count = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] == '-')
+			count = count + 1;
+		else if (_isdigit(str[i]) == 1)
+			break;
+		i = i + 1;
+	}
+	return (count);
 }
 
 /**
@@ -73,40 +100,31 @@ void rev_string(char *s)
 
 int _atoi(char *s)
 {
-	int i, j, k;
-	int minus;
+	int i;
 	int ans;
-	char first[98];
+	int minus = minus_counter(s);
+	char *start = skip_it(s);
 
-	j = 0;
-	while (s[j] != '\0')
-	{
-		if ((_isdigit(s[j - 1]) == 0) && (_isdigit(s[j]) == 1))
-		{
-			first[0] = s[j];
-			break;
-		}
-		j = j + 1;
-	}
 	i = 0;
-	minus = 0;
-	while (s[i] != '\0')
+	ans = 0;
+	while (start[i] != '\0')
 	{
-		if (s[i] == '-')
-			minus = minus + 1;
-		if ((_isdigit(s[i]) == 1) && (_isdigit(s[i + 1]) == 0))
+		if (_isdigit(start[i]) == 1)
 		{
-			first[i + 1] = '\0';
+		ans = (ans * 10) + (start[i] - '0');
+		}
+		else if (_isdigit(start[i]) == 0 && _isdigit(start[i - 1]) == 1)
+		{
 			break;
 		}
-		i = i + 1;
+			i = i + 1;
 	}
-	k = 0;
-	ans = 0;
-	while (first[k] != '\0')
+	if ((minus % 2) != 0)
 	{
-		ans = (ans * 10) + (first[k] - '0');
-		k = k + 1;
+		return (-1 * ans);
 	}
-	return (ans);
+	else
+	{
+		return (ans);
+	}
 }
