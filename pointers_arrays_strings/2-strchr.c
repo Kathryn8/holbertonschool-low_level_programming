657d122d509226530eb283c60ada6727f7b5142c
Kathryn6368 <6368@holbertonstudents.com>
2023-03-04 14:38:29 -0800

    add file

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr -  locates a character in a string
+ * @s: takes parameter pointer to a string
+ * @c: a character
+ *
+ * Return: pointer to the character
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+	char *ptr = NULL;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			ptr = &s[i];
+			break;
+		}
+		i = i + 1;
+	}
+	return (ptr);
+}
