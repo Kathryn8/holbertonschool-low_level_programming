38a7d952a8b2b41d0d3495840bc7db183de2ad58
Kathryn6368 <6368@holbertonstudents.com>
2023-03-04 22:56:18 -0800

    new approach

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,76 +1,27 @@
 #include "main.h"
-#include <stddef.h>
-#include <stdio.h>
 
 /**
- * _strchr -  locates a character in a string
- * @s: takes parameter pointer to a string
- * @c: a character
+ * _strstr - Locates a substring
+ * @haystack: String being searched
+ * @needle: Substring being searched for
  *
- * Return: pointer to the character
+ * Return: Pointer to beginning of located substring or NULL
  */
-
-char *_strchr(char *s, char c)
-{
-	int i;
-	char *ptr = NULL;
-
-	i = 0;
-	while (s[i] >= '\0')
-	{
-		if (s[i] == c)
-		{
-			ptr = &s[i];
-			break;
-		}
-		i = i + 1;
-	}
-	return (ptr);
-}
-
-/**
- * _strcmp  - compares two strings
- * @s1: destination string
- * @s2: source string
- *
- * Return: an integer
- */
-
-int _strcmp(char *s1, char *s2)
-{
-	while (*s1 != '\0' || *s2 != '\0')
-	{
-		if (*s1 != *s2)
-			return (*s1 - *s2);
-		s1 = s1 + 1;
-		s2 = s2 + 1;
-	}
-	return (0);
-}
-
-/**
- * _strstr - locates a substring.
- * @haystack: takes parameter pointer to a string s which will be searched
- * @needle: a substring that will be searched for in haystack
- *
- * Return: a pointer to the matching substring or NULL
- */
-
 char *_strstr(char *haystack, char *needle)
 {
-	char *substring;
-	int same;
-	int i;
+	int i, j;
 
-	i = 0;
-	while (haystack[i] != '\0')
+	if (*needle == '\0')
+		return (haystack);
+	for (i = 0; haystack[i]; ++i)
 	{
-		substring = _strchr(haystack, *(needle + i));
-		same = _strcmp(substring, needle);
-		if (same == 0)
-			return (substring);
-		else
-			i = i + 1;
+		if (haystack[i] == needle[0])
+		{
+			for (j = 0; haystack[i] == needle[j]; ++i, ++j)
+				;
+			if (needle[j] == '\0')
+				return (haystack + i - j);
+		}
 	}
-	return (NULL);
+	return (0);
 }
