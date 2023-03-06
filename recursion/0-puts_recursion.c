a77f35a151d3950d8039bac2ec22538d0b30b9a5
Kathryn6368 <6368@holbertonstudents.com>
2023-03-05 21:28:04 -0800

    add file

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strlen - length of a string
+ * @s: takes parameter pointer to a char
+ *
+ * Return: length of a string without null character
+ */
+
+int _strlen(char *s)
+{
+	int counter;
+
+	counter = 0;
+	while (s[counter] != '\0')
+	{
+		counter = counter + 1;
+	}
+	return (counter);
+}
+
+/**
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: takes parameter pointer to a char
+ *
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
