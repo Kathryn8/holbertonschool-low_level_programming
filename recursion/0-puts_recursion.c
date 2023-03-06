011de179d401b6cb4b826b6ddd3003172a36710a
Kathryn6368 <6368@holbertonstudents.com>
2023-03-05 21:29:38 -0800

    remove excess function

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,25 +1,6 @@
 #include "main.h"
 
 /**
- * _strlen - length of a string
- * @s: takes parameter pointer to a char
- *
- * Return: length of a string without null character
- */
-
-int _strlen(char *s)
-{
-	int counter;
-
-	counter = 0;
-	while (s[counter] != '\0')
-	{
-		counter = counter + 1;
-	}
-	return (counter);
-}
-
-/**
  * _puts_recursion - prints a string, followed by a new line.
  * @s: takes parameter pointer to a char
  *
