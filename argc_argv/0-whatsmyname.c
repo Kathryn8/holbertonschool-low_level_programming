3c566368e00ccb0ab18a1f63595d5521d7c04ae2
Kathryn6368 <6368@holbertonstudents.com>
2023-03-06 19:13:14 -0800

    adjustments for betty

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -3,13 +3,15 @@
 
 /**
  * main - Write a program that prints its name, followed by a new line.
+ * @argc: count of arguments
+ * @argv: array of pointers to strings that are arguments to the function
  *
  * Return: void
  */
 
-int main(int argc, char*argv[])
+int main(int argc, char *argv[])
 {
-        if (argc >= 1)
+	if (argc >= 1)
 		printf("%s\n", argv[0]);
 	return (0);
 }
