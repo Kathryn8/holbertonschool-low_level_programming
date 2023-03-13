5f046f25ddb0b9da282f1c81e9af72b9f682d3f1
Kathryn6368 <6368@holbertonstudents.com>
2023-03-12 17:40:51 -0700

    add file

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -20,7 +20,7 @@ char *_strdup(char *str)
 	i = 0;
 	while (str[i] != '\0')
 		i = i + 1;
-	new_str = (char*)malloc(i + 1);
+	new_str = (char *)malloc(i + 1);
 	if (new_str == NULL)
 		return (NULL);
 	j = 0;
