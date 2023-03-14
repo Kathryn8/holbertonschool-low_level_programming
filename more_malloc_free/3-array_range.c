01805cd43ab0e75f68f33198b8b3fa82a3b7e99f
Kathryn6368 <6368@holbertonstudents.com>
2023-03-14 14:25:21 -0700

    add file

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strlen - length of a string
+ * @s: takes parameter pointer to a char
+ *
+ * Return: length of a string without null character
+ */
+
+int _strlen(char *s)
+{
+	int counter;
+
+	counter = 0;
+	while (s[counter] != '\0')
+	{
+		counter = counter + 1;
+	}
+	return (counter);
+}
+
+/**
+ * array_range -  creates an array of integers
+ * @min: integer
+ * @max: integer
+ *
+ * Return: a pointer to newly created array
+ */
+
+int *array_range(int min, int max)
+{
+	int *new_arr;
+	int len;
+	int i;
+
+	if (min > max)
+		return (NULL);
+	len = max - min;
+	new_arr = malloc(sizeof(*new_arr) * len);
+	if (new_arr == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		new_arr[i] = min + i;
+		i = i + 1;
+	}
+	return (new_arr);
+}
