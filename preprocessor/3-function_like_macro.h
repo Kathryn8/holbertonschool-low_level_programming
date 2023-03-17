7449e127430ee62b1292c007a742fb48e0d5583b
Kathryn6368 <6368@holbertonstudents.com>
2023-03-16 17:04:22 -0700

    fix guard statemetns

--- preprocessor/3-function_like_macro.h
+++ preprocessor/3-function_like_macro.h
@@ -1,6 +1,10 @@
-#ifndef GRANDPARENT_H
-#define GRANDPARENT_H
+#ifndef __3_FUNCTION_LIKE_MACRO_H__
+#define __3_FUNCTION_LIKE_MACRO_H__
+
+#define TRUE 1
+#define FALSE 0
 
 #define ABS(x) (((x) > 0) ? (x) : (-1 * (x)))
+#define IS_ALPHA(c) ((c) >= 'a' || (c) <= 'z') ? TRUE : FALSE)
 
-#endif /* GRANDPARENT_H */
+#endif /* __3_FUNCTION_LIKE_MACRO_H__ */
