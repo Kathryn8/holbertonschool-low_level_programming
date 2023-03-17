dae7ca76b005163df7f0a9ad48d32216753c38b7
Kathryn6368 <6368@holbertonstudents.com>
2023-03-16 17:05:26 -0700

    fix guard statements

--- preprocessor/0-object_like_macro.h
+++ preprocessor/0-object_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef GRANDPARENT_H
-#define GRANDPARENT_H
+#ifndef __0_OBJECT_LIKE_MACRO_H__
+#define __0_OBJECT_LIKE_MACRO_H__
 
 #define SIZE 1024
 
-#endif /* GRANDPARENT_H */
+#endif /* __0_OBJECT_LIKE_MACRO_H__ */
