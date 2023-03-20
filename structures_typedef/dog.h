d3d3d5b29c4a0f574cc093bf6ad1ddf54aaae6e3
Kathryn6368 <6368@holbertonstudents.com>
2023-03-19 17:30:44 -0700

    add guards and struct comments

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,20 @@
+#ifndef __DOG_H__
+#define __DOG_H__
+
+/**
+ * struct dog - a furry four legged creature
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: A dog is the only thing on earth that loves you more than you
+ *  love yourself
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif /* __DOG_H__ */
