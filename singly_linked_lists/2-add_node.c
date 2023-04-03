c1b442adc78093c361dfe08da55dfbc346894886
Kathryn6368 <6368@holbertonstudents.com>
2023-04-02 22:32:18 -0700

    add file

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * _strlen - length of a string
+ * @s: takes parameter pointer to a char
+ *
+ * Return: length of a string
+ */
+
+int _strlen(char *s)
+{
+	int counter;
+
+	counter = 0;
+	while (s[counter] != '\0')
+	{
+		counter = counter + 1;
+	}
+	return (counter);
+}
+
+/**
+ * print_list - prints the data in linked list node
+ * @head: a pointer to a pointer to the first node
+ * @str: a string to be stored in the struct
+ *
+ * Return: address of the new element, or NULL if failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *temp;
+
+	temp = malloc(sizeof(temp));
+	if (temp == NULL)
+	{
+		return (NULL);
+	}
+	temp->str = strdup(str);
+	temp->len = _strlen((char *)str);
+	temp->next = *head;
+	*head = temp;
+	return (*head);
+}
