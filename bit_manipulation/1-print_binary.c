a332bd0f4f685a083bffbbbd2411efd21d6ce020
Kathryn6368 <6368@holbertonstudents.com>
2023-04-08 15:07:12 -0700

    add logic to handle ULONG_MAX

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -1,28 +1,34 @@
+#include <limits.h>
 #include "main.h"
 
+/**
+ * print_binary -  prints the binary representation of a number
+ * @n: unsigned long int
+ *
+ * Return: void
+ */
 void print_binary(unsigned long int n)
 {
-	unsigned long int mask = 1;
+	unsigned long int mask;
+	int printed = 0;
 
+	mask = 1UL << (sizeof(unsigned long int) * CHAR_BIT - 1);
 	if (n == 0)
 	{
 		_putchar('0');
 		return;
 	}
-	while ((mask << 1) <= n)
-	{
-		mask = mask << 1;
-	}
 	while (mask != 0)
 	{
-		if ((n & mask) == 0)
-        {
-		_putchar('0');
-        }
-		else
+		if ((n & mask) == 0 && printed == 1)
+		{
+			_putchar('0');
+		}
+		else if ((n & mask) != 0)
 		{
 			_putchar('1');
+			printed = 1;
 		}
-		mask = mask >> 1;
+		mask >>= 1;
 	}
 }
