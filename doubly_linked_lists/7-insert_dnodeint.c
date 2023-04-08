33fdaa8dd2a2f0ba4f7c7e60d60321b1bb9d676f
Kathryn6368 <6368@holbertonstudents.com>
2023-04-07 19:32:45 -0700

    completely change everything to align with chatgtps suggestions

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -13,41 +13,41 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new;
-	dlistint_t *ptr;
-	unsigned int i;
+    unsigned int i = 0;
+    dlistint_t *current = *h;
+    dlistint_t *new_node = NULL;
 
-	new = malloc(sizeof(*new));
-	if (new == NULL)
-	{
-		printf("Error - malloc failed\n");
-		return (NULL);
-	}
-	new->n = n;
-	new->prev = NULL;
-	new->next = NULL;
-	ptr = *h;
-	if (idx == 0)
-	{
-		return (add_dnodeint(h, n));
-	}
-	i = 0;
-	while ((ptr != NULL) && (i < idx))
-	{
-		ptr = ptr->next;
-		i = i + 1;
-	}
-	if ((idx == i) && (ptr == NULL))
-	{
-		*h = add_dnodeint_end(h, n);
-		printf("%d\n", (*h)->n);
-		return (*h);
-	}
-	if (i != idx || ptr == NULL)
-		return (NULL);
-	new->prev = ptr->prev;
-	new->next = ptr;
-	(ptr->prev)->next = new;
-	ptr->prev = new;
-	return (new);
+    new_node = malloc(sizeof(dlistint_t));
+    if (!new_node)
+        return NULL;
+    new_node->n = n;
+    new_node->prev = NULL;
+    new_node->next = NULL;
+    if (idx == 0)
+    {
+        new_node->next = *h;
+        if (*h != NULL)
+            (*h)->prev = new_node;
+        *h = new_node;
+        return new_node;
+    }
+    while (i < idx - 1)
+    {
+        if (current == NULL)
+        {
+            return NULL;
+        }
+        current = current->next;
+        i++;
+    }
+    if (current == NULL)
+    {
+        return NULL;
+    }
+    new_node->next = current->next;
+    new_node->prev = current;
+    if (current->next != NULL)
+        current->next->prev = new_node;
+    current->next = new_node;
+    return new_node;
 }
